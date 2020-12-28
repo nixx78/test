e06e7035c4f569f2170a593d3ee6380e06a9a238
nixx78 <nixx78@gmail.com>
2020-12-28 21:42:52 +0200

    AssertThrow sample added

--- junit5/src/main/java/lv/nixx/poc/junit5/service/Calculator.java
+++ junit5/src/main/java/lv/nixx/poc/junit5/service/Calculator.java
@@ -3,6 +3,11 @@ package lv.nixx.poc.junit5.service;
 public class Calculator {
 
 	public Integer add(Integer a, Integer b) {
+
+		if (a == null || b == null) {
+			throw new IllegalArgumentException("Argument can't be null");
+		}
+
 		return a + b;
 	}
 
--- junit5/src/test/java/lv/nixx/poc/junit5/CalculatorTest.java
+++ junit5/src/test/java/lv/nixx/poc/junit5/CalculatorTest.java
@@ -12,18 +12,19 @@ import lv.nixx.poc.junit5.service.Calculator;
 
 @DisplayName("Unit tests for Calculator class")
 @RunWith(JUnitPlatform.class)
-public class CalculatorAssertAllTest {
+public class CalculatorTest {
+
+    private final Calculator c = new Calculator();
 
     @Test
     @DisplayName("Test for all calculator operations")
     public void calculatorFullTest() {
 
-        final Calculator c = new Calculator();
 
         assertAll("Calculator Add tests",
                 () -> assertEquals((Integer) 7, c.add(3, 4)),
                 () -> assertEquals((Integer) 2, c.add(3, -1)),
-                () -> assertThrows(NullPointerException.class, () -> c.add(null, 1))
+                () -> assertThrows(IllegalArgumentException.class, () -> c.add(null, 1))
         );
 
         Executable[] ex = new Executable[]{
@@ -34,6 +35,13 @@ public class CalculatorAssertAllTest {
 
         assertAll("Calculator Subtract tests", ex);
 
+    }
+
+    @DisplayName("Null parameter test")
+    @Test
+    public void nullParameterTest() {
+        final IllegalArgumentException npe = assertThrows(IllegalArgumentException.class, () -> c.add(10, null));
+        assertEquals("Argument can't be null", npe.getMessage());
     }
 
 
