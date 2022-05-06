e78ce03cd23224b548a7f7f06c57bff720491ed7
nixx78 <nixx78@gmail.com>
2022-05-06 11:01:23 +0300

    DB Changes sample updated

--- assertj-poc/src/test/java/lv/nixx/poc/assertj/DBAssertionsTest.java
+++ assertj-poc/src/test/java/lv/nixx/poc/assertj/DBAssertionsTest.java
@@ -3,9 +3,8 @@ package lv.nixx.poc.assertj;
 import org.assertj.db.type.Changes;
 import org.assertj.db.type.Request;
 import org.assertj.db.type.Table;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.db.api.Assertions.assertThat;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @SpringBootTest
-@TestInstance(PER_CLASS)
 public class DBAssertionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(DBAssertionsTest.class);
@@ -31,70 +28,7 @@ public class DBAssertionsTest {
     @Autowired
     DataSource dataSource;
 
-    @Test
-    void tableSample() {
-
-        Table table = new Table(dataSource, "transactions");
-
-        assertThat(table).column("account_id")
-                .value().isEqualTo("ACC1")
-                .value().isEqualTo("ACC1")
-                .value().isEqualTo("ACC2")
-                .value().isEqualTo("ACC2");
-
-        assertThat(table).row(0)
-                .value().isEqualTo(1)
-                .value().isEqualTo("ACC1")
-                .value().isEqualTo(10.01);
-
-
-    }
-
-    @Test
-    void requestFromTableSample() {
-
-        // https://joel-costigliola.github.io/assertj/assertj-db-concepts.html#request
-
-        Request s = new Request(dataSource, "select * from transactions where account_id=?", "XACC");
-        assertThat(s).row(0)
-                .value().isEqualTo(5)
-                .value().isEqualTo("XACC")
-                .value().isEqualTo(50.03);
-    }
-
-    @Test
-    void changesInTableSample() {
-
-        // https://github.com/assertj/assertj-examples/blob/main/assertions-examples/src/test/java/org/assertj/examples/db/ChangesAssertionExamples.java
-
-        Changes changes = new Changes(new Table(dataSource, "transactions"));
-        changes.setStartPointNow();
-
-        jdbcTemplate.batchUpdate("INSERT INTO transactions(account_id, amount) VALUES (?,?)", List.of(
-                new Object[]{"NEW_ACC_1", 10.01},
-                new Object[]{"NEW_ACC_2", 10.02}
-        ));
-
-        changes.setEndPointNow();
-
-        assertThat(changes).hasNumberOfChanges(2)
-                .ofCreation().hasNumberOfChanges(2)
-                .ofModification().hasNumberOfChanges(0)
-                .ofDeletion().hasNumberOfChanges(0);
-
-        assertThat(changes)
-                .change()
-                    .isCreation()
-                    .hasPksNames("id")
-                    .hasPksValues(6)
-                .change()
-                    .isCreation()
-                    .hasPksNames("id")
-                    .hasPksValues(7);
-
-    }
-
-    @BeforeAll
+    @BeforeEach
     void prepareData() {
         log.info("Creating tables");
 
@@ -117,6 +51,84 @@ public class DBAssertionsTest {
         txns.forEach(t -> log.info("Inserting transactions record for {} {}", t[0], t[1]));
         jdbcTemplate.batchUpdate("INSERT INTO transactions(account_id, amount) VALUES (?,?)", txns);
     }
+
+    @Test
+    void tableSample() {
+
+        Table table = new Table(dataSource, "transactions");
+
+        assertThat(table).column("account_id")
+                .value().isEqualTo("ACC1")
+                .value().isEqualTo("ACC1")
+                .value().isEqualTo("ACC2")
+                .value().isEqualTo("ACC2");
+
+        assertThat(table).row(0)
+                .value().isEqualTo(1)
+                .value().isEqualTo("ACC1")
+                .value().isEqualTo(10.01);
+    }
+
+    @Test
+    void requestFromTableSample() {
+
+        // https://joel-costigliola.github.io/assertj/assertj-db-concepts.html#request
+
+        Request s = new Request(dataSource, "select * from transactions where account_id=?", "XACC");
+        assertThat(s).row(0)
+                .value().isEqualTo(5)
+                .value().isEqualTo("XACC")
+                .value().isEqualTo(50.03);
+    }
+
+    @Test
+    void changesInTableSample() {
+
+        // https://github.com/assertj/assertj-examples/blob/main/assertions-examples/src/test/java/org/assertj/examples/db/ChangesAssertionExamples.java
+
+        Changes changes = new Changes(new Table(dataSource, "transactions"));
+        changes.setStartPointNow();
+
+        modifyTable();
+
+        changes.setEndPointNow();
+
+        assertThat(changes).hasNumberOfChanges(4)
+                .ofCreation().hasNumberOfChanges(2)
+                .ofModification().hasNumberOfChanges(1)
+                .ofDeletion().hasNumberOfChanges(1);
+
+        assertThat(changes)
+                .change()
+                .isCreation()
+                    .hasPksValues(6)
+                .change()
+                    .isCreation()
+                    .hasPksValues(7)
+                .change()
+                    .isModification()
+                    .hasPksValues(1)
+                    .columnAmongTheModifiedOnes().valueAtEndPoint().isEqualTo("ACC_1_MOD")
+                    .columnAmongTheModifiedOnes().valueAtEndPoint().isEqualTo(99.99)
+                .change()
+                    .isDeletion()
+                    .hasPksValues(2);
+
+    }
+
+    private void modifyTable() {
+        jdbcTemplate.batchUpdate("INSERT INTO transactions(account_id, amount) VALUES (?,?)", List.of(
+                new Object[]{"NEW_ACC_1", 10.01},
+                new Object[]{"NEW_ACC_2", 10.02}
+        ));
+
+        List<Object[]> p = new ArrayList<>();
+        p.add(new Object[]{"ACC_1_MOD", 99.99, 1});
+        jdbcTemplate.batchUpdate("UPDATE transactions SET account_id = ?, amount=? WHERE id=?", p);
+
+        jdbcTemplate.batchUpdate("DELETE FROM transactions WHERE id=2");
+    }
+
 
 }
 
